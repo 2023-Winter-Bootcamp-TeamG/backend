3af9df782e15ed62f57c13c091e0b44c106ee269
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-01-08 14:47:38 +0900

    feat/ 회원가입 뷰 구현

--- member/views.py
+++ member/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import MemberRegistrationSerializer
 
-# Create your views here.
+class MemberRegistrationAPIView(APIView):
+    @swagger_auto_schema(
+        request_body=MemberRegistrationSerializer,
+        responses={201: '회원가입 완료', 400: '잘못된 요청'}
+    )
+    def post(self, request):
+        serializer = MemberRegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'nickname': serializer.validated_data.get('nickname'),
+                'message': '회원가입 완료.'
+            }, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
