f4ed3775d6f6712d272accd24cf688aa4073c069
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-01-10 00:29:50 +0900

    env/ S3 환경설정

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +140,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS = [
     'member.authentication.MemberAuthenticationBackend',
 ]
+
+# 환경 변수 파일 로드
+load_dotenv()
+
+# AWS S3 설정
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+
+# 정적 파일 저장을 위한 설정
+AWS_S3_STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_S3_STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# 미디어 파일 저장을 위한 설정
+AWS_S3_MEDIA_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'myproject.storage_backends.MediaStorage'
