e2c1924c5a6c51290076b3cad8cd59b8fdef1e25
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-01-12 11:08:01 +0900

    feat/ 사진 수정 url 설정

--- photo/urls.py
+++ photo/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PhotoManageView
-from .views import PhotoDetailView
+from .views import PhotoManageView, PhotoDetailView, PhotoUpdateView
 
 urlpatterns = [
     # 사진 저장/전체 불러오기
     path('api/v1/photos/', PhotoManageView.as_view(), name='photo-album'),
     path('api/v1/photos/<int:id>', PhotoDetailView.as_view(), name='photo-detail'),
+    path('api/v1/photos/update/<int:id>', PhotoUpdateView.as_view(), name='photo update'),
     ]
