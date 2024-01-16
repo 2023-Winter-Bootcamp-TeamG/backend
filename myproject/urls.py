7e1c2579a3780a60bc19b28c6a9b53b60d5154ce
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2024-01-16 20:18:44 +0900

    myproject url 수정

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include('photo.urls')),
-    path('', include('sticker.urls'))
+    path('', include('sticker.urls')),
+    path('', include('frame.urls'))
 ]
 
 
