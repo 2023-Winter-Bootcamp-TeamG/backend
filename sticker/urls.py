b994b1c5d833c02d6b5ded1adf14477eeedc6fe8
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2024-01-20 15:18:51 +0900

    ai 설계 수정

--- sticker/urls.py
+++ sticker/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # 스티커 저장, 불러오기 url
     path('api/v1/stickers/', StickerManageView.as_view(), name='manage sticker'),
     # 스티커 삭제 경로
-    path('api/v1/stickers/delete/<int:id>/', StickerDeleteView.as_view(), name='sticker-delete'),
+    path('api/v1/stickers/<int:id>/', StickerDeleteView.as_view(), name='sticker-delete'),
 ]
