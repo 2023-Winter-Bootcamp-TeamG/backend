e58b6eed4cf0789d7b116dc9f70cc3b6d32cd277
LeeYunseo04 <129928913+LeeYunseo04@users.noreply.github.com>
2024-01-21 15:37:29 +0900

    /

--- photo/urls.py
+++ photo/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     # 사진 저장/삭제/전체 불러오기
     path('api/v1/photos/', PhotoManageView.as_view(), name='photo-album'),
     # 사진 삭제
-    path('api/v1/photos/<int:id>', PhotoDeleteView.as_view(), name='photo-delete'),
+    path('api/v1/photos/<int:id>/', PhotoDeleteView.as_view(), name='photo-delete'),
     # 사진 수정
-    path('api/v1/photos/<int:id>', PhotoUpdateView.as_view(), name='photo update'),
+    path('api/v1/photos/<int:id>/', PhotoUpdateView.as_view(), name='photo update'),
     # 사진 상세보기
-    path('api/v1/photos/<int:id>', PhotoDetailView.as_view(), name='photo-detail'),
+    path('api/v1/photos/<int:id>/', PhotoDetailView.as_view(), name='photo-detail'),
     # qr 코드
-    path('api/v1/photos/qr/<int:id>', QRAPIView.as_view(), name='get qr code'),
+    path('api/v1/photos/qr/<int:id>/', QRAPIView.as_view(), name='get qr code'),
 
     ]
