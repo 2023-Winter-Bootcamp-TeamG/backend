f018bc66e771263b5cc19b507f6435ac381b192b
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-01-24 02:32:52 +0900

    [feat] document 생성

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from member.models import Member
 from django_prometheus.models import ExportModelOperationsMixin
+from djongo import models as djongo_models
 
 class Photo(ExportModelOperationsMixin('photo'), models.Model):
     member_id = models.ForeignKey(Member, on_delete=models.CASCADE, null=True)
@@ -12,5 +13,37 @@ class Photo(ExportModelOperationsMixin('photo'), models.Model):
     def __str__(self):
         return self.title
 
+############## Models for MongoDB ###############
+# 커스텀에 사용된 스티커
+class UsedSticker(djongo_models.Model):
+    url = djongo_models.URLField()
+    x = djongo_models.FloatField()
+    y = djongo_models.FloatField()
+    size = djongo_models.IntegerField()
 
-# Create your models here.
+    # 추상 모델 지정
+    class Meta:
+        abstract = True
+
+# 커스텀에 사용된 텍스트박스
+class TextBox(djongo_models.Model):
+    text = djongo_models.TextField()
+    x = djongo_models.FloatField()
+    y = djongo_models.FloatField()
+    size = djongo_models.IntegerField()
+    color = djongo_models.CharField(max_length=30)
+    font = djongo_models.CharField(max_length=30)
+
+    #추상 모델 지정
+    class Meta:
+        abstract = True
+
+# 커스텀 된 사진
+class CustomedPhoto(djongo_models.Model):
+    user_id = djongo_models.IntegerField()
+    photo_url = djongo_models.URLField()
+    stickers = djongo_models.ArrayField(model_container=UsedSticker)
+    textboxes = djongo_models.ArrayField(model_container=TextBox)
+
+    def __str__(self):
+        return self.photo_url
