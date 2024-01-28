adc16e8e30d231c457fac2bb352ec640f1d1fbc4
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-01-29 01:05:27 +0900

    [feat] Qr 코드 생성 로직 수정

--- photo/urls.py
+++ photo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PhotoManageView, PhotoEditView
-from .views import QRAPIView
+from .views import QRAPIView, QrPhotoView
 
 urlpatterns = [
     # 사진 저장/전체 불러오기
@@ -8,6 +8,6 @@ urlpatterns = [
     # 사진 수정/삭제/상세보기
     path('api/v1/photos/<int:id>/', PhotoEditView.as_view(), name='photo-edit'),
     # qr 코드
+    path('api/v1/photos/qr', QrPhotoView.as_view(), name='post photo for qr'),
     path('api/v1/photos/qr/<int:id>/', QRAPIView.as_view(), name='get qr code'),
-
     ]
