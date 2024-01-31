7a9b11de119f1511c7a009129244ff589d177679
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-01-31 14:09:32 +0900

    Update tasks.py

--- sticker/tasks.py
+++ sticker/tasks.py
@@ -48,8 +48,25 @@ def aisticker_create(keyword):
         size="1024x1024"
     )
 
-    # 생성된 이미지의 URL 추출
-    aisticker_url = url_response.data[0].url  # 이미지 url 출력
+    # # 생성된 이미지의 URL 추출
+    # aisticker_url = url_response.data[0].url  # 이미지 url 출력
+    # image_response = requests.get(aisticker_url)
+    # dalleimage = Image.open(BytesIO(image_response.content))
+    #
+    # # 이미지를 메모리에 임시로 저장하기 위한 스트림 생성
+    # buffered = BytesIO()
+    #
+    # # dalleimage를 BytesIO 스트림에 저장
+    # dalleimage.save(buffered, format="JPEG")
+    #
+    # img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    return url_response
+
+@shared_task
+def save_aisticker_model(img_str, member_id, is_ai):
+    ######################
+    aisticker_url = img_str.data[0].url  # 이미지 url 출력
     image_response = requests.get(aisticker_url)
     dalleimage = Image.open(BytesIO(image_response.content))
 
@@ -57,17 +74,12 @@ def aisticker_create(keyword):
     buffered = BytesIO()
 
     # dalleimage를 BytesIO 스트림에 저장
-    dalleimage.save(buffered, format="JPEG")
+    dalleimage.save(buffered, format="PNG")
 
-    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-    return img_str
-
-@shared_task
-def save_aisticker_model(img_str, member_id, is_ai):
-
+    img_result = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    #################################
     # 이미지 데이터 디코딩
-    input_image = base64.b64decode(img_str)
+    input_image = base64.b64decode(img_result)
 
     # 고유한 파일명 생성(S3는 같은 이름의 파일을 업로드할 시 덮어쓰기 때문)
     image_name = f"{uuid.uuid4()}.png"
